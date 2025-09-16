b76735d4c0ac3f767c27e1f4375a9251c825549e
umarmf343 <124717827+umarmf343@users.noreply.github.com>
2025-09-16 14:25:27 +0100

    Update class-vx-admin.php

--- admin/class-vx-admin.php
+++ admin/class-vx-admin.php
@@ -2,11 +2,13 @@
 if ( ! defined( 'ABSPATH' ) ) exit;
 
 class VX_Admin {
-    public function enqueue( $hook ) {
+    public function register_assets( $hook ) {
         $screen = get_current_screen();
         if ( isset( $screen->post_type ) && $screen->post_type === VXLITE_CPT ) {
-            wp_enqueue_style( 'vxlite-admin', VXLITE_URL . 'admin/css/admin.css', [], VXLITE_VERSION );
-            wp_enqueue_script( 'vxlite-admin', VXLITE_URL . 'admin/js/admin.js', [ 'jquery' ], VXLITE_VERSION, true );
+            wp_register_style( 'vxlite-admin', VXLITE_URL . 'admin/css/admin.css', [], VXLITE_VERSION );
+            wp_register_script( 'vxlite-admin', VXLITE_URL . 'admin/js/admin.js', [ 'jquery' ], VXLITE_VERSION, true );
+            wp_enqueue_style( 'vxlite-admin' );
+            wp_enqueue_script( 'vxlite-admin' );
         }
     }
 }
