c5e55d3d74315e02b81c5078a8c96e68eb06a211
umarmf343 <124717827+umarmf343@users.noreply.github.com>
2025-09-16 19:42:14 +0100

    Update class-vx.php

--- includes/class-vx.php
+++ includes/class-vx.php
@@ -14,15 +14,35 @@ class VX {
         $this->loader = new VX_Loader();
 
         // Subsystems
-        $i18n      = new VX_I18n();
-        $cpt       = new VX_CPT();
-        $shortcode = new VX_Shortcode();
+        $i18n       = new VX_I18n();
+        $cpt        = new VX_CPT();
+        $shortcode  = new VX_Shortcode();
 
-        // Load i18n
+        // i18n
         $this->loader->add_action( 'init', $i18n, 'load_textdomain' );
 
-        // Register CPT
+        // CPT
         $this->loader->add_action( 'init', $cpt, 'register_cpt' );
+
+        // Capabilities (roles)
+        if ( class_exists( 'VX_Capabilities' ) ) {
+            $caps = new VX_Capabilities();
+            $this->loader->add_action( 'init', $caps, 'add_caps' );
+            $this->loader->add_action( 'switch_theme', $caps, 'add_caps' ); // safety
+        }
+
+        // Settings page (Lite)
+        if ( is_admin() && class_exists( 'VX_Settings' ) ) {
+            $settings = new VX_Settings();
+            $this->loader->add_action( 'admin_init', $settings, 'register_settings' );
+            $this->loader->add_action( 'admin_menu', $settings, 'register_menu' );
+        }
+
+        // Upgrade notices (gentle)
+        if ( is_admin() && class_exists( 'VX_Upgrade_Notices' ) ) {
+            $up = new VX_Upgrade_Notices();
+            $this->loader->add_action( 'admin_notices', $up, 'maybe_show' );
+        }
 
         // Shortcode
         $this->loader->add_action( 'init', $shortcode, 'register' );
@@ -39,11 +59,13 @@ class VX {
             $this->loader->add_action( 'rest_api_init', $rest, 'register_routes' );
         }
 
-        // Admin
         if ( is_admin() ) {
             if ( class_exists( 'VX_Admin' ) ) {
                 $admin = new VX_Admin();
                 $this->loader->add_action( 'admin_enqueue_scripts', $admin, 'register_assets' );
+                // Add "Views" column for analytics
+                $this->loader->add_filter( 'manage_'.VXLITE_CPT.'_posts_columns', $admin, 'columns' );
+                $this->loader->add_action( 'manage_'.VXLITE_CPT.'_posts_custom_column', $admin, 'column_content', 10, 2 );
             }
             if ( class_exists( 'VX_Admin_Metabox' ) ) {
                 $this->loader->add_action( 'add_meta_boxes', [ 'VX_Admin_Metabox', 'register' ] );
@@ -57,11 +79,11 @@ class VX {
             }
             if ( class_exists( 'VX_Admin_Ajax' ) ) {
                 $ajax = new VX_Admin_Ajax();
-                $this->loader->add_action( 'admin_action_vxlite_duplicate', $ajax, 'duplicate_tour' ); // row action link
+                $this->loader->add_action( 'admin_action_vxlite_duplicate', $ajax, 'duplicate_tour' );
             }
         }
 
-        // Block (Gutenberg)
+        // Gutenberg
         if ( function_exists( 'register_block_type' ) && class_exists( 'VX_Block' ) ) {
             $block = new VX_Block();
             $this->loader->add_action( 'init', $block, 'register' );
@@ -71,6 +93,13 @@ class VX {
         if ( did_action('elementor/loaded') && class_exists( 'VX_Elementor' ) ) {
             $el = new VX_Elementor();
             $this->loader->add_action( 'elementor/widgets/register', $el, 'register_widgets' );
+        }
+
+        // Analytics (AJAX ping)
+        if ( class_exists( 'VX_Analytics' ) ) {
+            $an = new VX_Analytics();
+            $this->loader->add_action( 'wp_ajax_vxlite_ping', $an, 'ping' );
+            $this->loader->add_action( 'wp_ajax_nopriv_vxlite_ping', $an, 'ping' );
         }
     }
 
