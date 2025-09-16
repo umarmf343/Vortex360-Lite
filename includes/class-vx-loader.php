f65f61348a767841596e4a8a787906195dc611b9
umarmf343 <124717827+umarmf343@users.noreply.github.com>
2025-09-16 13:12:40 +0100

    Update class-vx-loader.php

--- includes/class-vx-loader.php
+++ includes/class-vx-loader.php
@@ -1,132 +1,16 @@
 <?php
-/**
- * The file that defines the core plugin class
- *
- * A class definition that includes attributes and functions used across both the
- * public-facing side of the site and the admin area.
- *
- * @link       https://vortex360.co
- * @since      1.0.0
- *
- * @package    Vortex360_Lite
- * @subpackage Vortex360_Lite/includes
- */
+if ( ! defined( 'ABSPATH' ) ) exit;
 
-// Prevent direct access
-if (!defined('ABSPATH')) {
-    exit;
-}
-
-/**
- * The core plugin class.
- *
- * This is used to define internationalization, admin-specific hooks, and
- * public-facing site hooks.
- *
- * Also maintains the unique identifier of this plugin as well as the current
- * version of the plugin.
- *
- * @since      1.0.0
- * @package    Vortex360_Lite
- * @subpackage Vortex360_Lite/includes
- * @author     Vortex360 Team <support@vortex360.co>
- */
 class VX_Loader {
+    private $actions = [];
 
-    /**
-     * The array of actions registered with WordPress.
-     *
-     * @since    1.0.0
-     * @access   protected
-     * @var      array    $actions    The actions registered with WordPress to fire when the plugin loads.
-     */
-    protected $actions;
-
-    /**
-     * The array of filters registered with WordPress.
-     *
-     * @since    1.0.0
-     * @access   protected
-     * @var      array    $filters    The filters registered with WordPress to fire when the plugin loads.
-     */
-    protected $filters;
-
-    /**
-     * Initialize the collections used to maintain the actions and filters.
-     *
-     * @since    1.0.0
-     */
-    public function __construct() {
-        $this->actions = array();
-        $this->filters = array();
+    public function add_action( $hook, $component, $callback, $priority = 10, $args = 1 ) {
+        $this->actions[] = compact( 'hook', 'component', 'callback', 'priority', 'args' );
     }
 
-    /**
-     * Add a new action to the collection to be registered with WordPress.
-     *
-     * @since    1.0.0
-     * @param    string               $hook             The name of the WordPress action that is being registered.
-     * @param    object               $component        A reference to the instance of the object on which the action is defined.
-     * @param    string               $callback         The name of the function definition on the $component.
-     * @param    int                  $priority         Optional. The priority at which the function should be fired. Default is 10.
-     * @param    int                  $accepted_args    Optional. The number of arguments that should be passed to the $callback. Default is 1.
-     */
-    public function add_action($hook, $component, $callback, $priority = 10, $accepted_args = 1) {
-        $this->actions = $this->add($this->actions, $hook, $component, $callback, $priority, $accepted_args);
-    }
-
-    /**
-     * Add a new filter to the collection to be registered with WordPress.
-     *
-     * @since    1.0.0
-     * @param    string               $hook             The name of the WordPress filter that is being registered.
-     * @param    object               $component        A reference to the instance of the object on which the filter is defined.
-     * @param    string               $callback         The name of the function definition on the $component.
-     * @param    int                  $priority         Optional. The priority at which the function should be fired. Default is 10.
-     * @param    int                  $accepted_args    Optional. The number of arguments that should be passed to the $callback. Default is 1
-     */
-    public function add_filter($hook, $component, $callback, $priority = 10, $accepted_args = 1) {
-        $this->filters = $this->add($this->filters, $hook, $component, $callback, $priority, $accepted_args);
-    }
-
-    /**
-     * A utility function that is used to register the actions and hooks into a single
-     * collection.
-     *
-     * @since    1.0.0
-     * @access   private
-     * @param    array                $hooks            The collection of hooks that is being registered (that is, actions or filters).
-     * @param    string               $hook             The name of the WordPress filter that is being registered.
-     * @param    object               $component        A reference to the instance of the object on which the filter is defined.
-     * @param    string               $callback         The name of the function definition on the $component.
-     * @param    int                  $priority         The priority at which the function should be fired.
-     * @param    int                  $accepted_args    The number of arguments that should be passed to the $callback.
-     * @return   array                                  The collection of actions and filters registered with WordPress.
-     */
-    private function add($hooks, $hook, $component, $callback, $priority, $accepted_args) {
-        $hooks[] = array(
-            'hook'          => $hook,
-            'component'     => $component,
-            'callback'      => $callback,
-            'priority'      => $priority,
-            'accepted_args' => $accepted_args
-        );
-
-        return $hooks;
-    }
-
-    /**
-     * Register the filters and actions with WordPress.
-     *
-     * @since    1.0.0
-     */
     public function run() {
-        foreach ($this->filters as $hook) {
-            add_filter($hook['hook'], array($hook['component'], $hook['callback']), $hook['priority'], $hook['accepted_args']);
-        }
-
-        foreach ($this->actions as $hook) {
-            add_action($hook['hook'], array($hook['component'], $hook['callback']), $hook['priority'], $hook['accepted_args']);
+        foreach ( $this->actions as $a ) {
+            add_action( $a['hook'], [ $a['component'], $a['callback'] ], $a['priority'], $a['args'] );
         }
     }
 }
