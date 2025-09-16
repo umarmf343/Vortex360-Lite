3de0c30b83a009e545b682ba8367329e2c9d6910
umarmf343 <124717827+umarmf343@users.noreply.github.com>
2025-09-16 14:22:13 +0100

    Update class-vx-i18n.php

--- includes/class-vx-i18n.php
+++ includes/class-vx-i18n.php
@@ -3,6 +3,10 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 
 class VX_I18n {
     public function load_textdomain() {
-        load_plugin_textdomain( 'vortex360-lite', false, dirname( plugin_basename( VXLITE_FILE ) ) . '/languages' );
+        load_plugin_textdomain(
+            'vortex360-lite',
+            false,
+            dirname( plugin_basename( VXLITE_FILE ) ) . '/languages'
+        );
     }
 }
